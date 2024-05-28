b82c99fe6c48a7d273e475a371dc08e9f9f7861e
Administrator <Administrator@PC40>
2024-05-29 00:00:45 +0900

    Updated Navbar layout and styles

--- my-next-app/src/app/components/navBar.tsx
+++ my-next-app/src/app/components/navBar.tsx
@@ -1,24 +1,27 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-4 shadow-md">
-      <div className="text-xl font-bold">Lyra</div>
-      <div className="flex space-x-4">
+    <nav className="flex justify-between items-center p-6 shadow-md bg-white">
+      <div className="flex items-center">
+        <Image src="/logo-black.png" alt="Lyra Logo" width={100} height={100} />
+      </div>
+      <div className="flex justify-between items-center space-x-6">
         <Link href="/" legacyBehavior>
-          <a>Home</a>
+          <a className="text-black">Home</a>
         </Link>
         <Link href="/products" legacyBehavior>
-          <a>Products</a>
+          <a className="text-black">Products</a>
         </Link>
         <Link href="/pricing" legacyBehavior>
-          <a>Pricing</a>
+          <a className="text-black">Pricing</a>
         </Link>
         <Link href="/about" legacyBehavior>
-          <a>About us</a>
+          <a className="text-black">About us</a>
         </Link>
         <Link href="/signup" legacyBehavior>
-          <a className="px-4 py-2 border border-teal-500 text-teal-500 rounded-lg hover:bg-teal-500 hover:text-white transition">
+          <a className="px-4 py-2 border border-teal-500 text-teal-500 rounded-full hover:bg-teal-500 hover:text-white transition">
             Sign up
           </a>
         </Link>
