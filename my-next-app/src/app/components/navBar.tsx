dfc05b2880c8b3d94fddc9bab2c0743ca5bfc73e
Administrator <Administrator@PC40>
2024-05-28 22:44:39 +0900

    Fix Link component issues

--- my-next-app/src/app/components/navBar.tsx
+++ my-next-app/src/app/components/navBar.tsx
@@ -5,19 +5,19 @@ export default function Navbar() {
     <nav className="flex justify-between items-center p-4 shadow-md">
       <div className="text-xl font-bold">Lyra</div>
       <div className="flex space-x-4">
-        <Link href="/">
+        <Link href="/" legacyBehavior>
           <a>Home</a>
         </Link>
-        <Link href="/products">
+        <Link href="/products" legacyBehavior>
           <a>Products</a>
         </Link>
-        <Link href="/pricing">
+        <Link href="/pricing" legacyBehavior>
           <a>Pricing</a>
         </Link>
-        <Link href="/about">
+        <Link href="/about" legacyBehavior>
           <a>About us</a>
         </Link>
-        <Link href="/signup">
+        <Link href="/signup" legacyBehavior>
           <a className="px-4 py-2 border border-teal-500 text-teal-500 rounded-lg hover:bg-teal-500 hover:text-white transition">
             Sign up
           </a>
