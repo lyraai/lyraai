9e0ee84048f454c9327090a51bfab4906f9b3873
lyraai <admin@lyrai.net>
2024-06-01 18:07:10 +0900

    Updated

--- my-next-app/pages/api/getToken.js
+++ my-next-app/pages/api/getToken.js
@@ -89,26 +89,3 @@ export default async function handler(req, res) {
     res.status(500).json({ error: error.message });
   }
 }
-
-
-// import { GoogleAuth } from 'google-auth-library';
-
-// export default async function handler(req, res) {
-//   try {
-//     const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
-//     const auth = new GoogleAuth({
-//       credentials,
-//       scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-//     });
-
-//     const client = await auth.getClient();
-//     const projectId = await auth.getProjectId();
-//     const url = 'https://flask-hello-world-ys6elaj32q-an.a.run.app';
-
-//     const result = await client.request({ url });
-//     res.status(200).json(result.data);
-//   } catch (error) {
-//     console.error('Error getting access token:', error);
-//     res.status(500).json({ error: 'Error getting access token' });
-//   }
-// }
